a35944b85397ba2081e11f62701bf18b2ac3d11f
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2025-11-11 14:15:46 +0700

    enhanced time greetings

--- app/src/main/java/com/neko/marquee/text/Greetings.kt
+++ app/src/main/java/com/neko/marquee/text/Greetings.kt
@@ -10,10 +10,7 @@ import androidx.appcompat.widget.AppCompatTextView
 import io.nekohasekai.sagernet.R
 import java.util.Calendar
 
-/**
- * Custom TextView that displays speech based on time
- * and automatically changes when the clock changes.
- */
+
 class Greetings @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
@@ -22,7 +19,6 @@ class Greetings @JvmOverloads constructor(
 
     private val timeChangeReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            // Will be called every time the hour changes, or when the time is changed
             if (intent?.action == Intent.ACTION_TIME_TICK ||
                 intent?.action == Intent.ACTION_TIME_CHANGED ||
                 intent?.action == Intent.ACTION_TIMEZONE_CHANGED
@@ -38,7 +34,6 @@ class Greetings @JvmOverloads constructor(
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        // Register the receiver for hourly updates.
         val filter = IntentFilter().apply {
             addAction(Intent.ACTION_TIME_TICK)
             addAction(Intent.ACTION_TIME_CHANGED)
@@ -50,7 +45,6 @@ class Greetings @JvmOverloads constructor(
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        // Avoid memory leaks
         context.unregisterReceiver(timeChangeReceiver)
     }
 
@@ -59,10 +53,10 @@ class Greetings @JvmOverloads constructor(
     private fun updateGreeting() {
         val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
         @StringRes val greetingResId = when (hour) {
-            in 4..8 -> R.string.uwu_greeting_morning
-            in 9..15 -> R.string.uwu_greeting_afternoon
-            in 16..20 -> R.string.uwu_greeting_evening
-            in 21..23 -> R.string.uwu_greeting_night
+            in 5..10 -> R.string.uwu_greeting_morning
+            in 11..14 -> R.string.uwu_greeting_afternoon
+            in 15..18 -> R.string.uwu_greeting_evening
+            in 19..23 -> R.string.uwu_greeting_night
             else -> R.string.uwu_greeting_late_night
         }
         setText(greetingResId)
