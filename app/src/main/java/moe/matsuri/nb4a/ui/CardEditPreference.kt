28426f2c2f3c5b0d60114127307664513e4e5ea5
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2025-11-15 10:47:17 +0700

    fix

--- app/src/main/java/moe/matsuri/nb4a/ui/CardEditPreference.kt
+++ app/src/main/java/moe/matsuri/nb4a/ui/CardEditPreference.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.TextView
 import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
+import androidx.preference.EditTextPreferenceDialogFragmentCompat
+import androidx.preference.PreferenceFragmentCompat
 import io.nekohasekai.sagernet.R
 
 class CardEditPreference @JvmOverloads constructor(
@@ -15,11 +18,13 @@ class CardEditPreference @JvmOverloads constructor(
 ) : EditTextPreference(context, attrs, defStyleAttr, defStyleRes) {
 
     init {
-        layoutResource = R.layout.uwu_banner_profile
+        widgetLayoutResource = R.layout.uwu_banner_profile
     }
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
+        
+        holder.itemView.isClickable = false
 
         val cardClick = holder.findViewById(R.id.on_click)
 
@@ -27,7 +32,7 @@ class CardEditPreference @JvmOverloads constructor(
             performClick()
         }
 
-        val summary = holder.findViewById(android.R.id.summary) as? TextView
-        summary?.text = text
+        val summaryText = holder.findViewById(android.R.id.summary) as? TextView
+        summaryText?.text = text
     }
 }
